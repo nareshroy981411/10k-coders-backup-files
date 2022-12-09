c759259ee13d65d1cf6a04591f2fb6ce506b1b2b
nareshroy <nareshbodasu226@gmail.com>
2022-12-09 13:57:14 +0530

    pending

--- ReactJS/my-app/src/App.js
+++ ReactJS/my-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Coders from './CLASS components/Coders';
+import Components from './CLASS components/Components';
+import Directors from './CLASS components/Directors';
+import Games from './CLASS components/Games';
+import Movies from './CLASS components/Movies'
+import Musical from './CLASS components/Musical';
+import NareshRoy from './CLASS components/NareshRoy'
+import Producers from './CLASS components/Producers'
+import Stories from './CLASS components/Stories'
+import Villans from './CLASS components/Villans'
+import Company from './FUNCTIONAL components/Company'
+import Functional from './FUNCTIONAL components/Functional'
+import Paytm from './FUNCTIONAL components/Paytm'
+import Cashback from './FUNCTIONAL components/Cashback';
+import Property from './FUNCTIONAL components/Property';
+import Functional6 from './FUNCTIONAL components/Functional6';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Coders/>
+      <Components/>
+      <Directors/>
+      <Games/>
+      <Movies/>
+      <Musical/>
+      <NareshRoy/>
+      <Producers/>
+      <Stories/>
+      <Villans/>
+      <Company/>
+      <Functional/>
+      <Paytm/>
+      <Cashback/>
+      <Property/>
+      <Functional6/>
     </div>
   );
+  
 }
 
 export default App;
