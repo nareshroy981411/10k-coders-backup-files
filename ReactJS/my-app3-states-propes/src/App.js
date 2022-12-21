d7c63c4be363ad9d3762f39abcf9c8ad06d0dc9a
nareshroy <nareshbodasu226@gmail.com>
2022-12-21 12:54:46 +0530

    life-cycle-forms

--- ReactJS/my-app3-states-propes/src/App.js
+++ ReactJS/my-app3-states-propes/src/App.js
@@ -4,24 +4,35 @@ import Students from './Components/Student';
 import Employee from './Components/Employes';
 import Header from './Components/Header';
 import MainClassComp from './Components/MainClassComp';
-import Person from './form-Components/Ravans';
+// import Person from './form-Components/Ravans';
 import Ravan from './form-Components/Ravans';
 import LifeCycleA from './React-life-cycles/LifeCycleA';
 import Practice from './React-life-cycles/Users';
-import User from './React-life-cycles/User'
+// import User from './React-life-cycles/User'
+import UsrsCrud from './react-functional-comps/UsrsCrud'
+import { User } from './react-functional-comps/User';
+import { Person } from './react-functional-comps/Person';
+import Employees from './react-functional-comps/Employees'
 
 function App() {
   return (
     <div className="App">
       {/* <Ravan/> */}
       {/* <User/> */}
+
       {/* <Students/>
       <Employee/>
       <Header/>
       <MainClassComp/> */}
+
       {/* <LifeCycleA/> */}
       {/* <Practice/> */}
+      {/* <User/> */}
+
       <User/>
+    <Person/>
+    <Employees/>
+    <UsrsCrud/>
     </div>
   );
 }
