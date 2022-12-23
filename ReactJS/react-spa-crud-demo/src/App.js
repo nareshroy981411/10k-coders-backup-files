16d1f1a8c84635a26f1c2d17125323269f120728
nareshroy <nareshbodasu226@gmail.com>
2022-12-23 18:12:33 +0530

    Nested Route demo

---
+++ ReactJS/react-spa-crud-demo/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Users from "./Component/Users";
+import {BrowserRouter,Route,Routes,} from 'react-router-dom'
+import Layout from "./Component/Layout";
+import EditUser from "./Component/EditUser";
+import Deleteuser from "./Component/Deleteuser";
+import CreateUser from "./Component/CreateUser";
+function App() {
+  return (
+    <div className="App">
+
+     <Users/>
+    </div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Layout/>}>
+    //       <Route path="/edit" element={<EditUser/>}/>
+    //       <Route path="/delete" element={<Deleteuser/>}/>
+    //       <Route path="/create" element={<CreateUser/>}/>
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+  );
+}
+
+export default App;
