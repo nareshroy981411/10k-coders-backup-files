d94f5ce47091d197e4b8de57fbb1d40d9a1bbbd9
nareshroy981411 <114374410+nareshroy981411@users.noreply.github.com>
2023-01-06 14:43:47 +0530

    react-redux-project demo

---
+++ ReactJS/react-redux-demo-project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './Components/User';
+import Person from './Components/Person';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <User/> */}
+     <Person/>
+    </div>
+  );
+}
+
+export default App;
