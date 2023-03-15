359e7e9a9c60eaa16a012661da2415046129d04e
nareshroy981411 <bodasunaresh226@gmail.com>
2023-03-15 15:49:36 +0530

    nothing

--- ReactJS/my-app3-states-propes/src/App.js
+++ ReactJS/my-app3-states-propes/src/App.js
@@ -4,11 +4,11 @@ import Students from './Components/Student';
 import Employee from './Components/Employes';
 import Header from './Components/Header';
 import MainClassComp from './Components/MainClassComp';
-// import Person from './form-Components/Ravans';
+import Person from './form-Components/Ravans';
 import Ravan from './form-Components/Ravans';
 import LifeCycleA from './React-life-cycles/LifeCycleA';
 import Practice from './React-life-cycles/Users';
-// import User from './React-life-cycles/User'
+import User from './React-life-cycles/User'
 import UsrsCrud from './react-functional-comps/UsrsCrud'
 import { User } from './react-functional-comps/User';
 import { Person } from './react-functional-comps/Person';
@@ -17,8 +17,8 @@ import Employees from './react-functional-comps/Employees'
 function App() {
   return (
     <div className="App">
-      {/* <Ravan/> */}
-      {/* <User/> */}
+      {/* <Ravan/>
+      <User/> */}
 
       {/* <Students/>
       <Employee/>
@@ -29,10 +29,10 @@ function App() {
       {/* <Practice/> */}
       {/* <User/> */}
 
-      <User/>
+      {/* <User/>
     <Person/>
     <Employees/>
-    <UsrsCrud/>
+    <UsrsCrud/> */}
     </div>
   );
 }
--- ReactJS/projects/app1/src/CLASS components/Directors.jsx
+++ ReactJS/projects/app1/src/CLASS components/Directors.jsx
@@ -6,3 +6,28 @@ class Directors extends Component{
     }
 }
 export default Directors
+
+// import React, { Component } from 'react'
+
+// export default class Directors extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {data: 'Nresh'}
+//     }
+//   render() {
+//     return (
+//       <div>
+//         <Child passData ={this.state.data}></Child>
+//       </div>
+//     )
+//   }
+// }
+// class Child extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h1>{this.props.passData}</h1>
+//             </div>
+//         )
+//     }
+// }
